807cd27bab0663471ecaefaae886659acc3b8d0d
Adam Nagy <adna01@betssongroup.com>
2019-09-11 10:59:42 +0200

    created NJ02 Task1 skeleton

---
+++ DotNETModules/CshFundemantals/IntExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CshFundemantals
+{
+    public static class IntExtensions
+    {
+        public static string ToRoman(this int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DotNETModules/Tests/RomanNumerals.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CshFundemantals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class RomanNumerals
+    {
+        [TestMethod]
+        public void Convert1()
+        {
+            string result = 1.ToRoman();
+            Assert.AreEqual("I", result);
+        }
+
+        [TestMethod]
+        public void Convert3()
+        {
+            string result = 3.ToRoman();
+            Assert.AreEqual("III", result);
+        }
+
+        [TestMethod]
+        public void Convert5()
+        {
+            string result = 5.ToRoman();
+            Assert.AreEqual("V", result);
+        }
+
+        [TestMethod]
+        public void Convert7()
+        {
+            string result = 7.ToRoman();
+            Assert.AreEqual("VII", result);
+        }
+    }
+}
