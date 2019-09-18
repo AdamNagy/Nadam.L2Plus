6d02997d32ed78d365a3f46e1736814eaf56824d
AdamNagy <nagy.quest@gmail.com>
2019-09-19 00:08:36 +0200

    adding notes

--- GraduateWork/Nadam.GraduateWork/Nadam.GraduateWork.Web/Controllers/AccountController.cs
+++ GraduateWork/Nadam.GraduateWork/Nadam.GraduateWork.Web/Controllers/AccountController.cs
@@ -9,5 +9,9 @@ namespace Nadam.GraduateWork.Web.Controllers
 {
     public class AccountController : ApiController
     {
+        // Login (self account, facebook, gmail, EPAM AD)
+        // Logout
+        // ForgotPassword
+        // ChangePassword
     }
 }
--- GraduateWork/Nadam.GraduateWork/Nadam.GraduateWork.Web/Controllers/AdminController.cs
+++ GraduateWork/Nadam.GraduateWork/Nadam.GraduateWork.Web/Controllers/AdminController.cs
@@ -9,5 +9,14 @@ namespace Nadam.GraduateWork.Web.Controllers
 {
     public class AdminController : ApiController
     {
+        // GetUsers
+        // LockUser
+        // EnableUser
+
+        // GetNumberOfNotes
+        // GetNumberOfUsers
+
+        // GetLog (last n day/hour, type)
+        // ClearLog
     }
 }
