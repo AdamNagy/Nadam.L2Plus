8a343f53d8e6542bcf38e30b96af8e24c92ba0a3
AdamNagy <nagy.quest@gmail.com>
2019-11-05 00:09:35 +0100

    refactor api, define db schema

---
+++ GraduateWork/Nadam.GraduateWork/GraduateNotes.DataAccess/GraduateNotesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraduateNotes.DataAccess
+{
+    class GraduateNotesContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public GraduateNotesContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=GraduateNotes;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ GraduateWork/Nadam.GraduateWork/GraduateNotes.DataAccess/Note.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace GraduateNotes.DataAccess
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Owner { get; set; }
+        public string Location { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime Created { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime LastUpdated { get; set; }
+    }
+}
