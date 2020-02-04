3f66a85950726713fa06e3bd8e067b6f668e77e8
Adam Nagy <adna01@betssongroup.com>
2020-02-04 17:12:16 +0100

    LINQ mep module tasks

---
+++ DotNETModules/NJ05-LINQ/EmployeesTasks.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJ05_LINQ
+{
+    public static class EmployeesTasks
+    {
+        static IEnumerable<Employee> employees = new[] {
+            new Employee { Name = "Bela", Salary = 400},
+            new Employee { Name = "Laszlo", Salary = 400},
+            new Employee { Name = "Hunor", Salary = 500},
+            new Employee { Name = "Aron", Salary = 500},
+            new Employee { Name = "Andras", Salary = 420},
+            new Employee { Name = "Csaba", Salary = 250},
+            new Employee { Name = "David", Salary = 300},
+            new Employee { Name = "Endre", Salary = 620},
+            new Employee { Name = "Ferenc", Salary = 350},
+            new Employee { Name = "Gabor", Salary = 410},
+            new Employee { Name = "Imre", Salary = 900},
+            new Employee { Name = "Janos", Salary = 600},
+            new Employee { Name = "Karoly", Salary = 700},
+        };
+
+        public static void Run()
+        {
+            SameSalary();
+        }
+
+        static void BiggestSalary()
+        {
+            var biggestSalary = employees.MaxByProperty(p => p.Salary);
+            Console.WriteLine(biggestSalary);
+        }
+
+        // TODO: check if possible to write 1 query
+        static void BelowAvarage()
+        {
+            var avg = employees.Average(p => p.Salary);
+            var lessThanAvg = employees.Where(p => p.Salary < avg);
+        }
+
+        static void SameSalary()
+        {
+            var query = employees.GroupBy(p => p.Salary)
+                .Where(q => q.Count() > 1)
+                .OrderBy(r => r.Key)
+                .ToDictionary(p => p.Key, q => q.OrderBy(r => r.Name).Select(s => s.Name));
+
+            foreach (var group in query)
+            {
+                Console.WriteLine($"group: {group.Key}");
+                foreach (var item in group.Value)                
+                    Console.WriteLine(item);                
+            }
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
--- DotNETModules/NJ05-LINQ/Program.cs
+++ DotNETModules/NJ05-LINQ/Program.cs
@@ -16,102 +16,15 @@ namespace NJ05_LINQ
 {
     class Program
     {
-        private static readonly string[] strings =
-        {
-            "You only live forever in the lights you make",
-            "When we were young we used to say",
-            "That you only hear the music when your heart begins to break",
-            "Now we are the kids from yesterday",
-            "ca cab cat bolt bab bottom after alter"
-        };
+
 
         static void Main(string[] args)
         {
-            WordCounts();
-            Vowels();
-            LongestWord();
-            AvgWordCount();
-            RemoveDuplicates();
+            // StringArrayTasks.Run();
+            EmployeesTasks.Run();
         }
 
-        private static void WordCounts()
-        {
-            var counts = strings
-                .Select(sentence => sentence.Split(' '))
-                .Select(words => words.Count())
-                .ToList();
 
-            foreach (var count in counts)
-                Console.Write($"{count} ");
-
-            Console.WriteLine();
-        }
-
-        private static void Vowels()
-        {
-            var VOWELS = new List<char>{'a', 'e', 'i', 'o', 'u'};
-
-            var selected = strings.Select(sentence => sentence.Split(' '))
-                .Select(words => 
-                    words.Where(word => VOWELS.Contains(word[0])));
-
-            foreach (var words in selected)
-            {
-                foreach (var word in words)
-                    Console.Write($"{word} ");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine();
-        }
-
-        private static void LongestWord()
-        {
-            var flatenned = strings
-                .Select(sentence => sentence.Split(' '))
-                .SelectMany(words => words);
-
-            var longest = flatenned.MaxByProperty(p => p.Length);
-            Console.Write($"longest: {longest} \n");
-        }
-
-        private static void AvgWordCount()
-        {
-            var avgWordCount = strings
-                .Select(sentence => sentence.Split(' '))
-                .Select(words => words.Count())
-                .Average();
-
-            Console.WriteLine($"Avarage word count: {avgWordCount}\n");
-        }
-
-        private static void RemoveDuplicates()
-        {
-            var distinctWords = strings
-                .Select(sentence => sentence.ToLower().Split(' '))
-                //.SelectMany(p => p)
-                .GroupBy(word => word[0]);
-                
-            // .Select(groups => groups.First());
-            // .OrderBy(words => words[0][0]);
-
-            foreach (var group in distinctWords)
-            {
-                foreach (var sentence in group)
-                {
-                    //Console.WriteLine($"{sentence.Key}: {word}");
-                    //foreach (var s in word)
-                    //{
-                    //    Console.Write($"{s} ");
-                    //}
-                }
-                // Console.WriteLine();
-            }
-
-
-            //var nums = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
-            //var s = nums.GroupBy(p => p % 2 == 0);
-        }
     }
 
     public static class EnumerableExtensions
